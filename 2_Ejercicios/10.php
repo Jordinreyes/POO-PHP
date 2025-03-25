0eec640d82087a0e48025358d983b83b3e777c32
Reyes <jordinp062@gmail.com>
2025-03-25 20:41:12 +0100

    10.php

---
+++ 2_Ejercicios/10.php
@@ -0,0 +1,10 @@
+Herencia con Métodos Abstractos
+Objetivo:
+
+Crea una clase abstracta Electrodomestico con el método abstracto encender().
+
+Crea dos clases Televisor y Refrigerador que hereden de Electrodomestico.
+
+Implementa encender() en cada una con un mensaje personalizado.
+
+Instancia Televisor y Refrigerador y prueba el método encender().
