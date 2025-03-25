05e577c87c4e067bd05f7d855ae6582a9b260c9b
Reyes <jordinp062@gmail.com>
2025-03-25 20:39:05 +0100

    9.php

---
+++ 2_Ejercicios/9.php
@@ -0,0 +1,11 @@
+ Herencia y Sobrecarga de Constructores
+Objetivo:
+
+Crea una clase Persona con propiedades nombre y edad.
+
+Usa un constructor para asignar estos valores.
+
+Crea una clase Estudiante que herede de Persona y agregue carrera.
+
+Modifica el constructor de Estudiante para recibir nombre, edad y carrera.
+
