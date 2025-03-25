28f715a449535c3895fd11ad3c92f9c101c2d9e3
Reyes <jordinp062@gmail.com>
2025-03-25 20:35:04 +0100

    Create 7.php

---
+++ 2_Ejercicios/7.php
@@ -0,0 +1,10 @@
+Herencia con Métodos Propios
+Objetivo:
+
+Crea una clase base Animal con la propiedad nombre y el método hacerSonido().
+
+Crea las clases Perro y Gato que hereden de Animal.
+
+Cada clase debe sobreescribir hacerSonido() con "Guau" y "Miau" respectivamente.
+
+Instancia ambas clases y llama a sus método
