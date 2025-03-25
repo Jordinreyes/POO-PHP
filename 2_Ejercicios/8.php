9c8bf6684831d8366c5e3cc78ce7f596259a3f35
Reyes <jordinp062@gmail.com>
2025-03-25 20:37:47 +0100

    Create 8.php

---
+++ 2_Ejercicios/8.php
@@ -0,0 +1,9 @@
+Crea una clase Empleado con la propiedad sueldoBase y un método calcularSueldo().
+
+Crea las clases Gerente y Vendedor que hereden de Empleado.
+
+Gerente tiene un bono del 20% sobre el sueldo base.
+
+Vendedor recibe comisión del 10%.
+
+Instancia ambas clases y muestra sus sueldos.
